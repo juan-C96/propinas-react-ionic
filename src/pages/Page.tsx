91cecf9fe895e93fb87bc7f7a53ac7dae9247c2a
Juan Carlos Hernandez <jchernandez9606@gmail.com>
2022-06-08 18:58:58 +0200

    ready v2

--- src/pages/Page.tsx
+++ src/pages/Page.tsx
@@ -1,6 +1,5 @@
 import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import { useParams } from 'react-router';
-import ExploreContainer from '../components/ExploreContainer';
 import Calcular from '../components/Calcular';
 import './Page.css';
 
