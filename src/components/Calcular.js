0f960a060f6c13ebc33b6ecd0dadcaa7ef8e1849
Juan Carlos Hernandez <jchernandez9606@gmail.com>
2022-06-15 00:15:11 +0200

    agregando exepciones y soporte android

--- src/components/Calcular.js
+++ src/components/Calcular.js
@@ -8,14 +8,20 @@ import { IonGrid, IonRow, IonCol } from '@ionic/react';
 class App extends Component {
 
     Operar() {
-        var resultado = APagar(parseInt(document.getElementById('total').value), parseInt(document.getElementById('comensales').value), parseInt(document.getElementById('propina').value));
-        if (isNaN(resultado))
-            alert('Ingrese valores para calcular el pago')
-        else
-            document.getElementById("result").value = resultado;
-        document.getElementById('total').value = null;
-        document.getElementById('comensales').value = null;
-        document.getElementById('propina').value = null;
+        if(parseInt(document.getElementById('comensales').value) <= 0){
+            alert('El número de comensales no puede ser menor o igual a 0.')
+        } else if (parseInt(document.getElementById('propina').value) < 0 || parseInt(document.getElementById('propina').value) > 100) {
+            alert('El porcentaje de propinas no puede ser menor a 0 o mayor a 100.')
+        } else {
+            var resultado = APagar(parseInt(document.getElementById('total').value), parseInt(document.getElementById('comensales').value), parseInt(document.getElementById('propina').value));
+            if (isNaN(resultado))
+                alert('Ingrese valores para calcular el pago')
+            else
+                document.getElementById("result").value = resultado;
+            document.getElementById('total').value = null;
+            document.getElementById('comensales').value = null;
+            document.getElementById('propina').value = null;
+        }
     }
 
     render() {
@@ -32,11 +38,11 @@ class App extends Component {
                                     <Segment stacked>
                                         <Form.Field>
                                             <IonLabel>Importe total de la cuenta:</IonLabel>
-                                            <IonInput type='number' id='total' fluid placeholder='Importe de cuenta'></IonInput>
+                                            <IonInput type='number' min={0} id='total' fluid placeholder='Importe de cuenta'></IonInput>
                                             <IonLabel>Número de comensales:</IonLabel>
-                                            <IonInput type='number' id='comensales' fluid placeholder='Número de comensales'></IonInput>
+                                            <IonInput type='number' min={1} id='comensales' fluid placeholder='Número de comensales'></IonInput>
                                             <IonLabel>Porciento a dejar en propina(Puede introducir cualquier valor de 0 a 100%):</IonLabel>
-                                            <IonInput type='number' id='propina' fluid placeholder='Porciento de propina a dejar'></IonInput>
+                                            <IonInput type='number' min={0} max={100} id='propina' fluid placeholder='Porciento de propina a dejar'></IonInput>
                                             <Button color='blue' fluid size='large' onClick={this.Operar}>
                                                 A pagar por comensales
                                             </Button>
